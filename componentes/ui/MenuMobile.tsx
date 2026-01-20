a80ab82cbb59ce0116d67a7d3f5b6aef6c8a3f00
devdavidias <davitdt206@gmail.com>
2026-01-20 18:24:00 -0300

    fix: adjust mobile menu layout and responsive modal positioning

--- componentes/ui/MenuMobile.tsx
+++ componentes/ui/MenuMobile.tsx
@@ -4,10 +4,7 @@ import {
   Dumbbell,
   Heart,
   House,
-  Utensils,
   Sparkles,
-  Plane,
-  ClipboardList,
   Settings,
 } from "lucide-react";
 import { useState } from "react";
@@ -20,19 +17,19 @@ export default function MenuMobile({ onChangeSessao }: MenuMobileProps) {
   const [activeSessao, setActiveSessao] = useState("dashboard");
 
   const menuItems = [
-    { id: "dashboard", label: "Início", icon: House },
-    { id: "estudos", label: "Estudos", icon: BookOpen },
-    { id: "treino", label: "Treino", icon: Dumbbell },
-    { id: "habitos", label: "Hábitos", icon: ClipboardList },
-    { id: "financas", label: "Finanças", icon: DollarSign },
-    { id: "beleza", label: "Beleza", icon: Sparkles },
-    { id: "viagens", label: "Viagens", icon: Plane },
-    { id: "casa", label: "CasaERotina", icon: House },
-    { id: "saude", label: "Saúde", icon: Heart },
-    { id: "alimentacao", label: "Alimentação", icon: Utensils },
-    { id: "Configuracao", label: "Configuracao", icon: Settings},
+    { id: "dashboard", icon: House },
+    { id: "estudos", icon: BookOpen },
+    { id: "treino", icon: Dumbbell },
+    { id: "financas",  icon: DollarSign },
+    { id: "saude", icon: Heart },
+    { id: "beleza", icon: Sparkles  },
+    
+    
+   
+    { id: "Configuracao",  icon: Settings},
   ];
 
+    
   return (
     <nav
       className="
@@ -47,23 +44,23 @@ export default function MenuMobile({ onChangeSessao }: MenuMobileProps) {
       "
     >
       <div className="flex">
-        {menuItems.map(({ id, label, icon: Icon }) => (
+        {menuItems.map(({ id,  icon: Icon }) => (
           <button
             key={id}
             onClick={() => {
               setActiveSessao(id);
               onChangeSessao(id);
             }}
-            className={`flex flex-col mt-[1em] items-center justify-center gap-1 px-4 transition
+            className={`flex flex-col mt-[1em] items-center justify-center gap-1 px-6 transition
               ${
                 activeSessao === id
                   ? "text-pink-500 font-semibold"
                   : "text-gray-700 hover:text-pink-500"
               }`}
-            aria-label={label}
+            
           >
-            <Icon size={22} />
-            <span className="text-[0.7em]">{label}</span>
+            <Icon size={25} />
+            
           </button>
         ))}
       </div>
